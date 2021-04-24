d789043357ab65dc54a2d7304db92b666c528d12
Petter Arnesen <parnesen@hotmail.com>
2021-04-24 21:31:22 +0200

    cleanup + module files

---
+++ PSPokeAPI.psm1
@@ -0,0 +1,6 @@
+.  $PSScriptRoot\Functions\Get-Pokemon.ps1
+.  $PSScriptRoot\Functions\Get-PokemonByType.ps1
+.  $PSScriptRoot\Functions\Get-PokemonLocation.ps1
+.  $PSScriptRoot\Functions\Get-PokemonMove.ps1
+.  $PSScriptRoot\Functions\Get-RandomPokemon.ps1
+.  $PSScriptRoot\Functions\Get-RandomPokemonMove.ps1
